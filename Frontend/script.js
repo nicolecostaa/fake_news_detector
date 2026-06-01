cb8ef348bb60fc427739114873c95e05db4f0458
cairo <cairo@gmabhjxbjs.com>
2026-06-01 11:13:43 -0300

    em processo

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,51 +1,104 @@
-async function analyzenws() {
+async function analyzeNews() {
 
     const text = document.getElementById("news").value;
 
     if(text.trim ()==="") {
-        alert?("Digite uma notícia.");
+        alert("Digite uma notícia.");
         return;
     
     }  
 
     const resultDiv =document.getElementById("result");
 
-    resultDiv.innerHTML =
-    <div class="loading">
-        🧠 IA analisando notícia...
-    </div>
-    ;
+    resultDiv.innerHTML = `
+        <div class="loading">
+            🧠 IA analisando notícia...
+        </div>
+    `;
 
     try {
          
         const response = await fetch(
             "http://127.0.0.1:5000/predict",
-        {  
-            method: "POST",
+            {  
+                method: "POST",
 
-            headers: {
-                "Content-type": "application/json"
-            } ,
+                headers: {
+                    "Content-type": "application/json"
+                } ,
 
-            body: JSON.stringify({
-                text: text
-            })
+                body: JSON.stringify({
+                    text: text
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        if(data.error) {
+
+            resultDiv.innerHTML = `
+                <div class="result-title fake">
+                    ❌ Erro
+                </div>
+
+                <p>${data.error}</p>
+            `;
+
+        return;
         }
 
-     );
-
-     const data = await response.json();
-
-     if(data.error) {
+        const resultClass =
+            data.result.includes("Fake")
+            ? "Fake"
+            : "real";
 
         resultDiv.innerHTML = `
-            <div class="result-title fake">
-                ❌ Erro
+            <div class="result-title ${resultClass}">
+                ${data.result}
             </div>
 
-            <p>${data.error}</p>
+            <div class="stats">
+                
+                <div class=stat-card>
+                    <div class="stat-label">
+                    🔴 Fake news
+                </div>
+
+                <div class="stat value">
+                    ${data.fake_probability}%
+                </div>
+            </div>
+
+            <div class="stat-card">
+                <div class="stat-label">
+                    🟢 Notícia real
+                </div>
+
+                <div class="stat-value">
+                    ${data.true_probability}%
+                    </div>
+                </div>
+
+            </div>
+            
+            <div class="explanation">
+                📖 ${data.explanation}
+            </div>
         `;
 
-       return;
+    } catch(error) {
 
-  }
+        console.error(error);
+
+        resultDiv.innerHTML =  `
+            <div class="result-title  fake">
+                ❌ Erro ao conectar
+            </div>
+
+            <p>
+                Não foi possível conectar com a API.
+            </p>
+        `;
+    }
+}
